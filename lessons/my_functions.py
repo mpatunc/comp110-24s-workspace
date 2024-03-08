f9304cb82d0b8224694824fb87d697895bcccd19
mahi <mspat@unc.edu>
2024-03-08 01:18:52 -0500

    my_functions lessons backup

---
+++ lessons/my_functions.py
@@ -0,0 +1,18 @@
+"""Define function and variables being imported"""
+
+def add(x: int, y: int) -> int: 
+    return x + y 
+
+if  __name__ == "__main__":
+    print(add(1,2))
+
+# def double(x: int) -> int:
+#     return x * 2
+
+# def double_display(y: int):
+#     print(y * 2)
+
+# double_display(2)
+    
+# if __name__ == "__main__":
+#     print(double(3))
